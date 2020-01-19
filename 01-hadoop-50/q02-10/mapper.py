ff2f8ea0b6b97c9535b38f28907f6ef5c12c2781
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2020-01-19 14:32:42 -0500

    Update mapper.py

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -2,9 +2,13 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-if __name__ == "__main__":    
+if __name__ == "__main__":
     for line in sys.stdin:
-      tipo= line.split(',')[4]
-      valor= line.split(',')[5]
-      
-      sys.stdout.write("{}\t1{}\n".format(tipo,valor))
+        purpose = ""
+        amount = ""
+        line = line.strip()
+        splits = line.split(",")
+        if splits[3] != 'purpose':
+            purpose = splits[3]
+            amount = splits[4]
+            print(purpose + "\t" + amount)
