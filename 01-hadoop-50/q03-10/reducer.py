8b5fb0a86e716d716328b74b6dd2bdc0572216f8
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2020-01-25 09:30:10 -0500

    Update reducer.py

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -4,16 +4,8 @@ import sys
 #
 if __name__ == '__main__':
 
-
-
-
     for line in sys.stdin:
+        valor, key, valor2 = line.split("\t")
+        valor2=int(valor2)
 
-        val, key, val2 = line.split("\t")
-
-        val2=int(val2)
-
-
-
-
-        sys.stdout.write("{},{}\n".format(key, val2))
+        sys.stdout.write("{},{}\n".format(key, valor2))
